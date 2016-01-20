198ef7c275ad17847c5e289f6c89e194665b287a
Peter Haak <info@pixelpeter.de>
2016-01-20 07:52:56 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+All Notable changes for the Laravel 5 WooCommerce REST API Client  will be documented in this file
+
+## 1.0.0
+- Stable first release
---
+++ src/WoocommerceServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Pixelpeter\Woocommerce;
+
+use Automattic\WooCommerce\Client;
+use Illuminate\Support\ServiceProvider;
+
+class WoocommerceServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/woocommerce.php' => config_path('woocommerce.php'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $app = $this->app;
+
+        // merge default config
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/woocommerce.php',
+            'woocommerce'
+        );
+
+        $config = $app['config']->get('woocommerce');
+
+        $app['woocommerce.client'] = $app->share(function() use ($config) {
+           return new Client(
+               $config['store_url'],
+               $config['consumer_key'],
+               $config['consumer_secret'],
+               [
+                   'version' => $config['api_version']
+               ]);
+        });
+
+        $app->singleton('Pixelpeter\Woocommerce\WoocommerceClient', function ($app) {
+            return new WoocommerceClient($app['woocommerce.client']);
+        });
+
+        $app->alias('Pixelpeter\Woocommerce\WoocommerceClient', 'woocommerce');
+    }
+}
