56652e29eb1dd685b35243ff52cbc140376a5926
pixelpeter <info@pixelpeter.de>
2017-01-29 22:57:42 +0100

    Add pagination functionality (#15)
    * Add methods to get http request/response headers fro the last api call
    
    * Add pagination functionality and tests
    
    * Update README and CHANGELOG

--- src/WoocommerceClient.php
+++ src/WoocommerceClient.php
@@ -70,4 +70,146 @@ class WoocommerceClient
     {
         return $this->client->delete($endpoint, $parameters);
     }
+
+    /**
+     * Get the http request header from the last request
+     *
+     * @return \Automattic\WooCommerce\HttpClient\Request
+     */
+    public function getRequest()
+    {
+        return $this->client->http->getRequest();
+    }
+
+    /**
+     * Get the http response headers from the last request
+     *
+     * @return \Automattic\WooCommerce\HttpClient\Response
+     */
+    public function getResponse()
+    {
+        return $this->client->http->getResponse();
+    }
+
+    /**
+     * Return the first page number of the result
+     *
+     * @return int
+     */
+    public function firstPage()
+    {
+        return 1;
+    }
+
+    /**
+     * Return the last page number of the result
+     *
+     * @return int
+     */
+    public function lastPage()
+    {
+        return $this->totalPages();
+    }
+
+    /**
+     * Return the current page number of the result
+     *
+     * @return int
+     */
+    public function currentPage()
+    {
+        return !empty($this->getRequest()->getParameters()['page']) ? $this->getRequest()->getParameters()['page'] : 1;
+    }
+
+    /**
+     * Return the total number of results
+     *
+     * @return int
+     */
+    public function totalResults()
+    {
+        return (int)$this->getResponse()->getHeaders()['X-WP-Total'];
+    }
+
+    /**
+     * Return the total number of pages for this result
+     *
+     * @return mixed
+     */
+    public function totalPages()
+    {
+        return (int)$this->getResponse()->getHeaders()['X-WP-TotalPages'];
+    }
+
+    /**
+     * Return the previous page number for the current page
+     * Will be null if there's no previous page
+     *
+     * @return int|null
+     */
+    public function previousPage()
+    {
+        $previous_page = $this->currentPage() - 1;
+        if ($previous_page < 1) {
+            $previous_page = null;
+        }
+
+        return $previous_page;
+    }
+
+    /**
+     * Return the next page number for the current page
+     * Will be null if there's no next page
+     *
+     * @return int|null
+     */
+    public function nextPage()
+    {
+        $next_page = $this->currentPage() + 1;
+        if ($next_page > $this->totalPages()) {
+            $next_page = null;
+        }
+
+        return $next_page;
+    }
+
+    /**
+     * Returns true if there's a next page
+     *
+     * @return bool
+     */
+    public function hasNextPage()
+    {
+        return (bool)$this->nextPage();
+    }
+
+    /**
+     * Returns true if there's a previous page
+     *
+     * @return bool
+     */
+    public function hasPreviousPage()
+    {
+        return (bool)$this->previousPage();
+    }
+
+    /**
+     * Returns true if there's no next page
+     *
+     * @return bool
+     */
+    public function hasNotNextPage()
+    {
+        return (bool)!$this->nextPage();
+    }
+
+    /**
+     * Returns true if there's no previous page
+     *
+     * @return bool
+     */
+    public function hasNotPreviousPage()
+    {
+        return (bool)!$this->previousPage();
+    }
 }
--- tests/WoocommerceClient.php
+++ tests/WoocommerceClient.php
@@ -2,6 +2,7 @@
 
 use Mockery;
 use PHPUnit_Framework_TestCase;
+use Pixelpeter\Woocommerce\Facades\Woocommerce;
 use Pixelpeter\Woocommerce\WoocommerceClient;
 
 /**
@@ -15,6 +16,9 @@ class WoocommerceClientTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->client = Mockery::mock('Automattic\WooCommerce\Client');
+        $this->httpClient = Mockery::mock('Automattic\WooCommerce\HttpClient\HttpClient');
+        $this->response = Mockery::mock('Automattic\WooCommerce\HttpClient\response');
+
         $this->woocommerce = new WoocommerceClient($this->client);
     }
 
@@ -77,6 +81,197 @@ class WoocommerceClientTest extends PHPUnit_Framework_TestCase
             ->andReturn('foo');
 
         $this->assertEquals($this->woocommerce->delete('someurl'), 'foo');
+    }
+
+    /**
+     * @test
+     */
+    public function getrequest_can_be_called()
+    {
+        $this->httpClient
+            ->shouldReceive('getRequest')
+            ->once()
+            ->andReturn('foo');
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->getRequest(), 'foo');
+    }
+
+    /**
+     * @test
+     */
+    public function getresponse_can_be_called()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse')
+            ->once()
+            ->andReturn('foo');
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->getResponse(), 'foo');
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_first_page_returns_valid_page_number()
+    {
+        $this->assertEquals($this->woocommerce->firstPage(), 1);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_last_page_returns_valid_page_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse->getHeaders')
+            ->once()
+            ->andReturn([
+                'X-WP-TotalPages' => 12
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->lastPage(), 12);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_current_page_returns_first_page_when_empty()
+    {
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->once()
+            ->andReturn([]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->currentPage(), 1);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_current_page_returns_valid_page_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->andReturn([
+                'page' => 6
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->currentPage(), 6);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_total_results_returns_valid_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse->getHeaders')
+            ->once()
+            ->andReturn([
+                'X-WP-Total' => 1234
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->totalResults(), 1234);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_total_pages_returns_valid_page_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse->getHeaders')
+            ->once()
+            ->andReturn([
+                'X-WP-TotalPages' => 13
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->lastPage(), 13);
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_previous_page_is_null_when_on_first_page()
+    {
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->andReturn([
+                'page' => 1
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertNull($this->woocommerce->previousPage());
+        $this->assertFalse($this->woocommerce->hasPreviousPage());
+        $this->assertTrue($this->woocommerce->hasNotPreviousPage());
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_previous_page_returns_valid_page_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->andReturn([
+                'page' => 5
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->previousPage(), 4);
+        $this->assertTrue($this->woocommerce->hasPreviousPage());
+        $this->assertFalse($this->woocommerce->hasNotPreviousPage());
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_next_page_is_null_when_on_last_page()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse->getHeaders')
+            ->andReturn([
+                'X-WP-TotalPages' => 13
+            ]);
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->andReturn([
+                'page' => 13
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertNull($this->woocommerce->nextPage());
+        $this->assertFalse($this->woocommerce->hasNextPage());
+        $this->assertTrue($this->woocommerce->hasNotNextPage());
+    }
+
+    /**
+     * @test
+     */
+    public function pagination_next_page_returns_valid_page_number()
+    {
+        $this->httpClient
+            ->shouldReceive('getResponse->getHeaders')
+            ->andReturn([
+                'X-WP-TotalPages' => 13
+            ]);
+        $this->httpClient
+            ->shouldReceive('getRequest->getParameters')
+            ->andReturn([
+                'page' => 5
+            ]);
+        $this->client->http = $this->httpClient;
+
+        $this->assertEquals($this->woocommerce->nextPage(), 6);
+        $this->assertTrue($this->woocommerce->hasNextPage());
+        $this->assertFalse($this->woocommerce->hasNotNextPage());
     }
 
     /**
